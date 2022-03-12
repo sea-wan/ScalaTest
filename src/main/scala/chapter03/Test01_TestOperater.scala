e690015f42a100d03d6163c83ce3a578213ae346
wan <wanyingfeisee@outlook.com>
2022-03-12 22:40:03 +0800

    关系运算符

--- src/main/scala/chapter03/Test01_TestOperater.scala
+++ src/main/scala/chapter03/Test01_TestOperater.scala
@@ -9,10 +9,15 @@ object Test01_TestOperater {
     val result2 : Double = 10 / 3
     println(result2)
 
-    val result3 : Double = (10.0 / 3)
+    val result3 : Double = 10.0 / 3
     println(result3.formatted("%.2f"))
 
     val result4 = 10 % 3
     println(result4)
+
+    val s1 : String = "hello"
+    val s2 : String = new String("hello")
+    println(s1 == s2)
+    println(s1.eq(s2))  //判断地址值
   }
 }
